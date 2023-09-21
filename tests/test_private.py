057477eb3f58d1905100f04a0b50ce88e38d24b5
Shengyi Liu <142550022+lshengyi152@users.noreply.github.com>
2023-09-21 03:00:55 -0700

    Update test_private.py

--- tests/test_private.py
+++ tests/test_private.py
@@ -9,45 +9,37 @@ from lab4 import file_extension, generate_frequency_map, palindrome_finder
 @pytest.mark.parametrize(
     "base_filename, freq_map",
     [
-        ("numbers_1.txt", [11, 11, 11, 11, 11, 11, 11, 11, 11, 11]),
-        ("numbers_2.txt", [4, 7, 10, 9, 10, 7, 7, 2, 3, 0]),
-        ("numbers_3.txt", [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]),
-        ("numbers_4.txt", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ("numbers_test_1.txt", [8, 7, 7, 7, 6, 6, 6, 6, 7, 8]),
+        ("numbers_test_2.txt", [4, 3, 0, 1, 3, 4, 3, 0, 2, 0]),
+        ("numbers_test_3.txt", [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+        ("numbers_test_4.txt", [32, 26, 14, 18, 24, 28, 24, 12, 22, 16]),
     ],
 )
 @pytest.mark.timeout(0.3)
 def test_generate_frequency_map(base_filename, freq_map):
     assert generate_frequency_map(base_filename) == freq_map
-
+    
+@pytest.mark.timeout(0.3)
+def test_generate_frequency_map_exception():
+    with pytest.raises(Exception):
+        generate_frequency_map("numbers_test_5.txt")
 
 @pytest.mark.parametrize(
     "base_filename, result",
     [
         (
-            "sentences_1.txt",
-            [
-                "radar",
-                "anna,kayak",
-                "civic",
-                "level",
-                "did,deed",
-                "hannah",
-                "dad",
-                "madam",
-                "racecar",
-                "",
-                "rotator",
-            ],
+            "sentences_test_1.txt",
+            ["did,kayak","refer","racecar","","hannah"],
         ),
         (
-            "sentences_2.txt",
-            ["racecar", "", "did", "radar", "level", "refer"],
+            "sentences_test_2.txt",
+            ["","","","",""],
         ),
         (
-            "sentences_3.txt",
-            ["deified", "kayak", "redivider", "noon", "madam", "civic"],
+            "sentences_test_3.txt",
+            ["deified,noon","dad,elle","repaper","",""],
         ),
-        ("sentences_4.txt", []),
+        ("sentences_test_4.txt", []),
     ],
 )
 @pytest.mark.timeout(0.3)
@@ -64,37 +56,37 @@ def test_palindrome_finder(base_filename, result):
 @pytest.mark.timeout(0.3)
 def test_palindrome_finder_exception():
     with pytest.raises(Exception):
-        palindrome_finder("sentences_5.txt")
+        palindrome_finder("sentences_test_5.txt")
 
 
 @pytest.mark.parametrize(
     "base_filename, result",
     [
         (
-            "extensions_1.txt",
+            "extensions_test_1.txt",
             {
-                "txt": ["first.txt", "second.txt", "third.txt"],
-                "doc": ["first.doc", "second.doc", "third.doc"],
-                "pdf": ["first.pdf", "second.pdf", "third.pdf"],
+                "txt": ["Shopping_List.txt", "Poetry_Collection.txt", "Recipe_Book.txt"],
+                "doc": ["Project_Proposal.doc", "Resume_Template.doc", "Meeting_Minutes.doc"],
+                "pdf": ["Report_Summary.pdf", "Vacation_Plan.pdf", "Budget_Spreadsheet.pdf"],
             },
         ),
         (
-            "extensions_2.txt",
+            "extensions_test_2.txt",
             {
-                "txt": ["first.txt"],
-                "doc": ["first.doc"],
-                "pdf": ["first.pdf", "second.pdf"],
+                "txt": [],
+                "doc": ["Presentation_Slides.doc", "Job_Application.doc", "Newsletter.doc"],
+                "pdf": ["Business_Plan.pdf", "Research_Paper.pdf","Travel_Itinerary.pdf"],
             },
         ),
         (
-            "extensions_3.txt",
+            "extensions_test_3.txt",
             {
-                "txt": ["second.txt", "third.txt", "first.txt"],
-                "doc": ["first.doc", "third.doc", "second.doc"],
-                "pdf": ["first.pdf", "second.pdf"],
+                "txt": ["Important_Notes.txt", "Personal_Journal.txt", "User_Manual.txt", "Contact_List.txt"],
+                "doc": [],
+                "pdf": [],
             },
         ),
-        ("extensions_4.txt", {"pdf": [], "doc": [], "txt": []}),
+        ("extensions_test_4.txt", {"pdf": [], "doc": [], "txt": []}),
     ],
 )
 @pytest.mark.timeout(0.3)
@@ -116,4 +108,4 @@ def test_file_extension(base_filename, result):
 @pytest.mark.timeout(0.3)
 def test_file_extension_exception():
     with pytest.raises(Exception):
-        file_extension("extensions_5.txt")
+        file_extension("extensions_test_5.txt")
